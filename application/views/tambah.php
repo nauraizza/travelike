7b4014feeaa511bcbe7d4a5689b89db7aa7e22ce
nauraizza <nauraizza99@gmail.com>
2018-04-25 19:45:35 +0700

    first commitr

---
+++ application/views/tambah.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<form action="http://localhost/travelike/codeigniter/index.php/User/prosesadd" method="post">
+		<input type="text" name="username">
+		<input type="text" name="password">
+		<input type="text" name="fullname">
+		<input type="text" name="level">
+		<input type="submit" name="kirim" value="Kirim">
+
+	</form>
+</body>
+</html>
